852aa9060a1bb75c3d3a6fff9fc5bcc1c0f91de3
Vini <benvini8@gmail.com>
2021-05-02 01:48:13 +0300

    add paging

--- ts/screens/MainScreen/components/MainScreen.tsx
+++ ts/screens/MainScreen/components/MainScreen.tsx
@@ -1,7 +1,7 @@
-import React, {FunctionComponent, useState, useEffect} from 'react';
-import {Screen, Typography} from '../../../shared/components';
+import React, { FunctionComponent, useState, useEffect, useCallback } from 'react';
+import { Screen, Typography } from '../../../shared/components';
 import axios from 'axios';
-import {FlatList, View, Text} from 'react-native';
+import { FlatList, View, Text, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
 import { apiHost } from '../../../../bin/config';
 import Card from '../../../shared/components/Card';
 
@@ -20,43 +20,100 @@ interface Product {
 }
 
 const MainScreen: FunctionComponent = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
+ 
+  useEffect(() => { // fetch first 10 products on initial render
+    addProducts();
+  }, []);
 
-  const fetchInitialProducts = async () => {
+  const addProducts = useCallback(async () => {
+    setIsLoading(true);
     try {
-      const products = await axios.get(apiHost);
-      setProducts(products.data);
+      const productsLength = products.length;
+      const productsToFetch = await axios.get(apiHost, {
+          params: { start: productsLength, end: (productsLength + 10) }
+      });
+      if (productsToFetch.data.length < 10) {
+        setIsLastPage(true);
+      }
+      setIsLoading(false);
+      setProducts(products => {
+        return [...products, ...productsToFetch.data]
+      });
     }
-    catch(e) {
+    catch (e) {
       console.log('err', e);
     }
-  }
+  }, [products]);
 
-  useEffect(() => {    
-    fetchInitialProducts();
-    }, []);
+  const loadMoreBtnWithActivityIndicator = () => {
+    return (
+      <View style={styles.footer}>
+        {!isLastPage &&
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={addProducts}
+            style={styles.loadMoreBtn}>
+            <Text style={styles.btnText}>Load More</Text>
+            {isLoading ? (
+              <ActivityIndicator
+                color="white"
+                style={{ marginLeft: 8 }} />
+            ) : null}
+          </TouchableOpacity>
+        }
+      </View>
+    );
+  };
 
   return (
     <Screen>
       <Typography>Test</Typography>
-      {products ? 
-                  <Card margin={20} padding={10} alignItems="center"><FlatList
-                      data={products}
-                      renderItem={({item}: { item: Product }) => (
-                          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
-                              <Typography>{item.name}</Typography>
-                          </View>)
-                      }
-                      numColumns={1}
-                  />
-                  </Card>
-                :
-                  <View style={{flexDirection: 'row', paddingVertical: 10, justifyContent: 'flex-start', alignItems: 'center'}}>
-                      <Text>Loading products...</Text>
-                  </View>
+      {products ?
+        <Card margin={20} padding={10} alignItems="center">
+          <FlatList
+            data={products}
+            renderItem={({ item }: { item: Product }) => (
+              <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+                <Typography>{item.name}</Typography>
+              </View>)
+            }
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={1}
+            ListFooterComponent={loadMoreBtnWithActivityIndicator}
+          />
+        </Card>
+        :
+        <View style={{ flexDirection: 'row', paddingVertical: 10, justifyContent: 'flex-start', alignItems: 'center' }}>
+          <Text>Loading products...</Text>
+        </View>
       }
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  footer: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  loadMoreBtn: {
+    padding: 10,
+    backgroundColor: '#3f51b5',
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+})
 
 export default MainScreen;
