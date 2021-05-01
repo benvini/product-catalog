857f77842acc25b2b6dfa1c3e81c3668a6f6a855
Vini <benvini8@gmail.com>
2021-05-02 02:44:35 +0300

    change styled to use styled-components/native

--- ts/screens/MainScreen/components/MainScreen.tsx
+++ ts/screens/MainScreen/components/MainScreen.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent, useState, useEffect, useCallback } from 'react';
 import { Screen, Typography } from '../../../shared/components';
 import axios from 'axios';
-import { FlatList, View, Text, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
+import { FlatList, ActivityIndicator } from 'react-native';
 import { apiHost } from '../../../../bin/config';
 import Card from '../../../shared/components/Card';
+import styled from 'styled-components/native';
 
 interface Product {
   categoryName: string;
@@ -19,11 +20,33 @@ interface Product {
   price: number;
 }
 
+const ProductContainer = styled.View`
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 8px;
+`;
+
+const LoadMoreContainer = styled.View`
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`;
+const LoadMoreBtn = styled.TouchableOpacity`
+    padding: 10px;
+    background-color: #3f51b5;
+    border-radius: 4px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
 const MainScreen: FunctionComponent = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isLastPage, setIsLastPage] = useState(false);
- 
+
   useEffect(() => { // fetch first 10 products on initial render
     addProducts();
   }, []);
@@ -33,7 +56,7 @@ const MainScreen: FunctionComponent = () => {
     try {
       const productsLength = products.length;
       const productsToFetch = await axios.get(apiHost, {
-          params: { start: productsLength, end: (productsLength + 10) }
+        params: { start: productsLength, end: (productsLength + 10) }
       });
       if (productsToFetch.data.length < 10) {
         setIsLastPage(true);
@@ -50,21 +73,21 @@ const MainScreen: FunctionComponent = () => {
 
   const loadMoreBtnWithActivityIndicator = () => {
     return (
-      <View style={styles.footer}>
+      <LoadMoreContainer>
         {!isLastPage &&
-          <TouchableOpacity
+          <LoadMoreBtn
             activeOpacity={0.9}
             onPress={addProducts}
-            style={styles.loadMoreBtn}>
-            <Text style={styles.btnText}>Load More</Text>
+          >
+            <Typography>Load More</Typography>
             {isLoading ? (
               <ActivityIndicator
                 color="white"
                 style={{ marginLeft: 8 }} />
             ) : null}
-          </TouchableOpacity>
+          </LoadMoreBtn>
         }
-      </View>
+      </LoadMoreContainer>
     );
   };
 
@@ -76,9 +99,9 @@ const MainScreen: FunctionComponent = () => {
           <FlatList
             data={products}
             renderItem={({ item }: { item: Product }) => (
-              <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+              <ProductContainer>
                 <Typography>{item.name}</Typography>
-              </View>)
+              </ProductContainer>)
             }
             keyExtractor={(item, index) => index.toString()}
             numColumns={1}
@@ -86,34 +109,10 @@ const MainScreen: FunctionComponent = () => {
           />
         </Card>
         :
-        <View style={{ flexDirection: 'row', paddingVertical: 10, justifyContent: 'flex-start', alignItems: 'center' }}>
-          <Text>Loading products...</Text>
-        </View>
+        <Typography>Loading products...</Typography>
       }
     </Screen>
   );
 };
-
-const styles = StyleSheet.create({
-  footer: {
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  loadMoreBtn: {
-    padding: 10,
-    backgroundColor: '#3f51b5',
-    borderRadius: 4,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  btnText: {
-    color: 'white',
-    fontSize: 15,
-    textAlign: 'center',
-  },
-})
 
 export default MainScreen;
